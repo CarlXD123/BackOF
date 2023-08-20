3575079a017ea464fc513b49aae13eb5d3382b1a
CarlXD123 <64506247+CarlXD123@users.noreply.github.com>
2023-08-20 00:58:10 -0500

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,15 +1,22 @@
 import mysql from "promise-mysql";
 import config from "../config";
 
-const connection=mysql.createConnection({
+const pool = mysql.createPool({
     host: config.host,
     database: config.database,
     user: config.user,
-    password: config.password
+    password: config.password,
+    connectionLimit: 10 // Puedes ajustar este valor según las necesidades de tu aplicación
 });
 
-const getConnection = () =>{
-    return connection;
+const getConnection = async () => {
+    try {
+        const connection = await pool.getConnection();
+        connection.release(); // Devuelve la conexión al pool después de obtenerla
+        return pool;
+    } catch (error) {
+        throw error;
+    }
 };
 
 module.exports = {
